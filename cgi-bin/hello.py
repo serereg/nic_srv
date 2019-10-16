8488ffdc1542d91b88dd0307f41593a9f43b9dea
serereg <56562368+serereg@users.noreply.github.com>
2019-10-16 08:42:57 +0300

    Add files via upload

---
+++ cgi-bin/hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+print("Content-type: text/html")
+print()
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  print("<h1>Hello world!</h1>")
+
+print("<h1>Hello world!</h1>")
